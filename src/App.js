9a4a039aa75d68412258729cb0d9e22dc3300417
amnonbb <amnonbb@gmail.com>
2022-04-14 12:22:56 +0300

    remux tools, hls and dash player

--- src/App.js
+++ src/App.js
@@ -1,105 +1,161 @@
 import React, { Component } from 'react';
-import { Container, Header, Content, Table, Col, Divider, FlexboxGrid, Panel, Toggle, SelectPicker } from 'rsuite';
+import { Container, Header, FlexboxGrid, Table, Col, Divider, Grid, Button, Row, SelectPicker } from 'rsuite';
 import 'rsuite/dist/rsuite.min.css';
 import './App.css';
-import {getData, REST_URL} from "./tools";
+import {getData, LANGS, putData, REST_URL, WEB_URL, WF_LANGUAGES} from "./tools";
+import ReactJWPlayer from "react-jw-player";
 
 class App extends Component {
 
-  state = {
-      dir_options: [{"key":"0","label":"Interesno (Russian)","value":"interesno"},{"key":"1","label":"Dlinnie (Russian)","value":"long"},{"key":"2","label":"Obuchalki (Russian)","value":"obuchalki"},{"key":"3","label":"A-NyamNyam (Russian)","value":"anyamnyam"},{"key":"4","label":"Sami (Russian)","value":"sami"},{"key":"5","label":"Drakoni (Russian)","value":"drakoni"},{"key":"6","label":"Fixiki (Russian)","value":"fixiki"},{"key":"7","label":"Shalosh-arba (Hebrew)","value":"34"},{"key":"8","label":"Sid-scince kid (Russian)","value":"sid"},{"key":"9","label":"Nuture (Russian)","value":"priroda"},{"key":"10","label":"Space (Russian)","value":"kosmos"},{"key":"11","label":"Clips (Mixed)","value":"clips"},{"key":"12","label":"Postman-Pat (English)","value":"pat"},{"key":"13","label":"Agada HaKosem (Hebrew)","value":"volshebnik"},{"key":"14","label":"Moi jivotnie (Russian)","value":"animals_friends"},{"key":"15","label":"Pepe (Russian)","value":"pepe"},{"key":"16","label":"Tut i Tam 1 (Russian)","value":"tut1"},{"key":"17","label":"Tut i Tam 2 (Russian)","value":"tut2"}],
-      files: [],
-      current_file: null,
-      current_dir: null,
-      i: 0,
-      loop: false,
-
-  };
+    state = {
+        disabled: false,
+        loading: false,
+        dir_options: [],
+        files: [],
+        name: null,
+        current_dir: null,
+        i: 0,
+        lang: null,
+        hls_url: "",
+        dash_url: "",
+        type: "",
+        url: "",
+        video: 2
+    };
 
     componentDidMount() {
-        getData(`options.json`, dir_options => {
+        getData(`${REST_URL}/chunks/list`, data => {
+            let dir_options = data.map((d,key) => {return({key,label: d.name, value: d.name})})
             this.setState({dir_options})
-        })
+        });
+        this.getFiles()
     };
 
-    getFiles = (dir) => {
-        getData(`${REST_URL}/${dir}/list`, files => {
+    getFiles = () => {
+        getData(`${REST_URL}/dl/list`, data => {
+            let files = data.map(f => {
+                let mhref = `${WEB_URL}/dl/${f.name}`;
+                f.link = (<a target="_blank" rel="noopener noreferrer" href={mhref}>{f.name}</a>)
+                return f
+            })
+            console.log(" :: get files: ", files);
             this.setState({files})
         })
-    }
-
-    selectFiles = (file, i) => {
-        const {name} = file;
-        let index = i.detail
-        //console.log(file, i)
-        const {current_dir, loop} = this.state;
-        this.setState({current_file: name, i: index});
-        let video = this.refs.video;
-        video.src = `${REST_URL}/data/video/${current_dir}/` + name;
-        video.play();
-        if(loop) {
-            video.onended = (index) => {
-                let {files} = this.state;
-                let c = index === files.length-1 ? 0 : index+1;
-                this.selectFiles(files[c], c)
-            }
-        }
     };
 
-    selectDir = (current_dir) => {
-        console.log(current_dir)
-        this.setState({current_dir});
-        this.getFiles(current_dir);
+    putRemux = () => {
+        this.setState({disabled: true, loading: true });
+        const {lang, name, video} = this.state;
+        const data = {language: lang, lang: WF_LANGUAGES[lang], name, video}
+        console.log(" :: Edit Meta: ", data);
+        putData(`${REST_URL}/exec/remux`, data, (cb) => {
+            console.log(":: PUT Respond: ",cb);
+            this.setState({disabled: false, loading: false });
+            this.getFiles()
+        });
+    };
+
+    selectLang = (lang) => {
+        console.log(lang)
+        this.setState({lang});
+    };
+
+    selectDir = (name) => {
+        console.log(name)
+        const {type} = this.state;
+        let hls_url = `${WEB_URL}/chunks/${name}/master.m3u8`;
+        let dash_url = `${WEB_URL}/chunks/${name}/manifest.mpd`;
+        const url = type === "dash" ? dash_url : hls_url;
+        this.setState({name, hls_url, dash_url, url});
+        //this.getFiles(current_dir);
+    };
+
+    selectType = (type) => {
+        this.setState({type, url: ""});
+    };
+
+    selectVideo = (video) => {
+        this.setState({video});
+    };
+
+    getFile = () => {
+        console.log("get file")
+    };
+
+    onPlaybackReady = () => {
+        // ...
+    };
+
+    render() {
+        let {files, video, lang, name, dir_options, url, loading, disabled} = this.state;
+        const styles = { width: 550, fontSize: 18 };
+
+        const rowClassName = rowData => {
+            if (rowData && rowData.name === name) {
+                return "active-row";
+            }
+            return "";
+        };
+
+        return (
+
+            <Container>
+                <Header align="middle">
+                    <h2>KMedia</h2>
+                    <Divider horizontal />
+                </Header>
+
+                <div className="show-grid">
+                    <FlexboxGrid justify="center">
+                        <FlexboxGrid.Item colspan={10}>
+                            <SelectPicker data={[
+                                {key:0, label:"HLS", value:"hls"},
+                                {key:1, label:"DASH", value:"dash"}
+                            ]} placeholder='Type' onChange={(value, e) => this.selectType(value)} />
+                            <Divider vertical />
+                            <SelectPicker size="lg" data={dir_options} placeholder='Stream' style={styles}
+                                          onChange={(value, e) => this.selectDir(value)} />
+                            <Divider horizontal />
+
+                            <ReactJWPlayer className="player"
+                                           customProps={{height: 360, width: 640}}
+                                           playerId='jSMsfFL2'
+                                           playerScript='https://cdn.jwplayer.com/libraries/RN2MAIg3.js'
+                                           playlist={[{
+                                               image: "",
+                                               sources: [{file: url}]
+                                           }]}
+                            />
+                        </FlexboxGrid.Item>
+                        <FlexboxGrid.Item colspan={10}>
+                            <SelectPicker data={[
+                                {key:0, label:"1080p", value:0},
+                                {key:1, label:"720p", value:1},
+                                {key:2, label:"360p", value:2},
+                                {key:3, label:"audio", value:3}
+                            ]} placeholder='Type' onChange={(value, e) => this.selectVideo(value)} value={video} />
+                            <Divider vertical />
+                            <SelectPicker size="lg" data={LANGS} placeholder='Stream' style={styles}
+                                          onChange={(value, e) => this.selectLang(value)} />
+                            <Divider horizontal />
+                            <Table bordered virtualized width={700} height={360} headerHeight={100} hover data={files}
+                                   onRowClick={this.getFile} rowClassName={rowClassName} style={{ cursor: 'pointer'}}>
+                                <Table.Column width={700} verticalAlign='middle'>
+                                    <Table.HeaderCell>
+                                        <Button block appearance="primary" loading={loading} disabled={disabled || !name || !lang} onClick={this.putRemux}>Remux</Button>
+                                    </Table.HeaderCell>
+                                    <Table.Cell dataKey="link" >
+
+                                    </Table.Cell>
+                                </Table.Column>
+                            </Table>
+                        </FlexboxGrid.Item>
+                    </FlexboxGrid>
+                </div>
+            </Container>
+
+        );
     }
-
-  render() {
-      let {files, current_file, dir_options, loop} = this.state;
-      const styles = { width: 224, fontSize: 18 };
-
-      const rowClassName = rowData => {
-          if (rowData && rowData.name === current_file) {
-              return "active-row";
-          }
-          return "";
-      };
-
-    return (
-
-        <Container>
-            <Header align="middle">
-                <Panel header={current_file?.split('.')[0] || "HomeTV"}  defaultExpanded style={{fontSize: 18, cursor: 'pointer'}}>
-                </Panel>
-            </Header>
-            <Content>
-                <FlexboxGrid justify="space-around" align="middle">
-                    <FlexboxGrid.Item as={Col} colspan={20} md={12}>
-                        <Panel bordered>
-                            <video ref="video" width={640} height={360} controls playsInline={true} />
-                        </Panel>
-                    </FlexboxGrid.Item>
-                    <FlexboxGrid.Item as={Col} colspan={20} md={12}>
-                        <Table virtualized height={400} headerHeight={100} hover data={files} bordered
-                               onRowClick={this.selectFiles} rowClassName={rowClassName} style={{fontSize: 18, cursor: 'pointer'}}>
-                            <Table.Column width={700} verticalAlign='middle'>
-                                <Table.HeaderCell>
-                                    <SelectPicker size="lg" data={dir_options} placeholder='Dir options' style={styles}
-                                                  onChange={(value, e) => this.selectDir(value)} />
-                                    <Divider vertical />
-                                    <Toggle onChange={() => this.setState({loop: !loop})}
-                                              checked={loop} />
-                                </Table.HeaderCell>
-                                <Table.Cell dataKey="name" >
-
-                                </Table.Cell>
-                            </Table.Column>
-                        </Table>
-                    </FlexboxGrid.Item>
-                </FlexboxGrid>
-            </Content>
-        </Container>
-
-    );
-  }
 }
 
 export default App;
--- src/tools.js
+++ src/tools.js
@@ -10,3 +10,89 @@ export const getData = (url, cb) => fetch(`${url}`,{
     }
 })
     .catch(ex => console.log(`get ${url}`, ex));
+
+export const putData = (url, data, cb) => fetch(`${url}`, {
+    method: 'PUT',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body:  JSON.stringify(data)
+})
+    .then((response) => {
+        if (response.ok) {
+            return response.json().then(respond => cb(respond));
+        }
+    })
+    .catch(ex => console.log("Put Data error:", ex));
+
+export const MDB_LANGUAGES = {
+    en: 'eng',
+    he: 'heb',
+    ru: 'rus',
+    es: 'spa',
+    it: 'ita',
+    de: 'ger',
+    nl: 'dut',
+    fr: 'fre',
+    pt: 'por',
+    tr: 'tur',
+    pl: 'pol',
+    ar: 'arb',
+    hu: 'hun',
+    fi: 'fin',
+    lt: 'lit',
+    ja: 'jpn',
+    bg: 'bul',
+    ka: 'geo',
+    no: 'nor',
+    sv: 'swe',
+    hr: 'hrv',
+    zh: 'chi',
+    fa: 'far',
+    ro: 'ron',
+    hi: 'hin',
+    mk: 'mkd',
+    sl: 'slv',
+    lv: 'lav',
+    sk: 'slk',
+    cs: 'cze',
+    ua: 'ukr',
+    zz: 'mlt',
+    xx: 'unk',
+};
+
+export const WF_LANGUAGES = function() {
+    let ret = {};
+    for(let key in MDB_LANGUAGES) {
+        ret[MDB_LANGUAGES[key]] = key;
+    }
+    return ret;
+}()
+
+export const LANGS = [{ key: 'he', value: 'heb', flag: 'il', label: 'Hebrew' },
+    { key: 'ru', value: 'rus', flag: 'ru', label: 'Russian' },
+    { key: 'en', value: 'eng', flag: 'us', label: 'English' },
+    { key: 'es', value: 'spa', flag: 'es', label: 'Spanish' },
+    { key: 'fr', value: 'fre', flag: 'fr', label: 'French' },
+    { key: 'it', value: 'ita', flag: 'it', label: 'Italian' },
+    { key: 'de', value: 'ger', flag: 'de', label: 'German' },
+    { key: 'tr', value: 'trk', flag: 'tr', label: 'Turkish' },
+    { key: 'pt', value: 'por', flag: 'pt', label: 'Portuguese' },
+    { key: 'bg', value: 'bul', flag: 'bg', label: 'Bulgarian' },
+    { key: 'ka', value: 'geo', flag: 'ge', label: 'Georgian' },
+    { key: 'ro', value: 'ron', flag: 'ro', label: 'Romanian' },
+    { key: 'hu', value: 'hun', flag: 'hu', label: 'Hungarian' },
+    { key: 'sv', value: 'swe', flag: 'se', label: 'Swedish' },
+    { key: 'lt', value: 'lit', flag: 'lt', label: 'Lithuanian' },
+    { key: 'hr', value: 'hrv', flag: 'hr', label: 'Croatian' },
+    { key: 'ja', value: 'jpn', flag: 'jp', label: 'Japanese' },
+    { key: 'sl', value: 'slv', flag: 'si', label: 'Slovenian' },
+    { key: 'pl', value: 'pol', flag: 'pl', label: 'Polish' },
+    { key: 'no', value: 'nor', flag: 'no', label: 'Norwegian' },
+    { key: 'lv', value: 'lav', flag: 'lv', label: 'Latvian' },
+    { key: 'ua', value: 'ukr', flag: 'ua', label: 'Ukrainian' },
+    { key: 'nl', value: 'dut', flag: 'nl', label: 'Dutch' },
+    { key: 'cn', value: 'chn', flag: 'cn', label: 'Chinese' },
+    { key: 'et', value: 'amh', flag: 'et', label: 'Amharic' },
+    { key: 'in', value: 'hin', flag: 'in', label: 'Hindi' },
+    { key: 'ir', value: 'per', flag: 'ir', label: 'Persian' },];
